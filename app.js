a3a74b18beb632f5912746af0906b1e113c389c7
mahic <mahic@domain.com>
2022-11-05 19:30:07 -0600

    finished

---
+++ app.js
@@ -0,0 +1,22 @@
+let item_names = [`Lays`, `Cheetos`, `Ruffels`];
+let item_prices = [`2`, `4`, `3`];
+let in_stock = [true, false, true];
+
+//console.log(item_names, item_prices, in_stock);
+
+let first_item_name = `lays`;
+let first_item_price = 2;
+
+item_names.push (`Pringles`);
+item_prices.push (`4`);
+in_stock.push (true);
+
+//console.log(item_names, item_prices, in_stock);
+
+let last_item_name = item_names.pop();
+let last_item_price = item_prices.pop();
+let last_item_stock = in_stock.pop();
+
+// console.log(item_names, item_prices, in_stock);
+
+
